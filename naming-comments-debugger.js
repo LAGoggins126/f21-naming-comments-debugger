be92e8767227711f8fd1e979f3718e72cbc6bffd
Luka Goggins <lukarowangoggins@computingforall.org>
2021-09-24 13:48:27 -0700

    finished assignment

--- naming-comments-debugger.js
+++ naming-comments-debugger.js
@@ -3,26 +3,30 @@
 
 /* Uncomment the below instruction for the
      debugger to pause execution of all JavaScript */
-// debugger;
+debugger;
 
 /* (1) Rename these identifiers to label what the function
          is doing and what the variables are holding
    (2) Add a comment above the function describing what it does
 
    Note: This is the Pythagorean theorem */
-function z(y,x)
+
+   //pythag + sqrt
+function pythagoreanTheorem(num1,num2)
 {
-	let w = (y * y) + (x * x);
-	let v = Math.sqrt(w);
-	return v;
+	let sum = (num1 * num1) + (num2 * num2);
+	let sqrt = Math.sqrt(sum);
+	return sqrt;
 }
 
 /* (3) Add Single Line Comments after each of the three
      instructions below to tell us what each "w" value
      is inside the method being called */
-console.log(z(3, 4));
-console.log(z(7, 21));
-console.log(z(17, 14));
+
+// adds value to variables
+console.log(pythagoreanTheorem(3, 4)); //w = 25
+console.log(pythagoreanTheorem(7, 21)); // w = 490
+console.log(pythagoreanTheorem(17, 14)); // w = 485
 
 // This is an example of what the above lines should look like
 console.log(z(10, 5)); // w = 125
